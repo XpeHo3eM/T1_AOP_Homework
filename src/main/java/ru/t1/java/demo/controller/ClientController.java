b35794477d6fd0a616d0ddf1a8c4274684f44c4f
Никулин Павел <npv90@mail.ru>
2025-01-24 17:24:04 +0400

    Заготовки для запуска кафки

--- src/main/java/ru/t1/java/demo/controller/ClientController.java
+++ src/main/java/ru/t1/java/demo/controller/ClientController.java
@@ -1,37 +1,28 @@
 package ru.t1.java.demo.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.t1.java.demo.aop.HandlingResult;
-import ru.t1.java.demo.aop.Track;
-import ru.t1.java.demo.aop.LogException;
-import ru.t1.java.demo.exception.ClientException;
+import ru.t1.java.demo.dto.ClientDto;
+import ru.t1.java.demo.kafka.KafkaClientProducer;
 import ru.t1.java.demo.service.ClientService;
-
-import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/client")
 @Slf4j
 public class ClientController {
 
     private final ClientService clientService;
+    private final KafkaClientProducer producer;
 
-    @LogException
-    @Track
-    @GetMapping(value = "/client")
-    @HandlingResult
-    public void doSomething() throws IOException, InterruptedException {
-//        try {
-//            clientService.parseJson();
-        Thread.sleep(3000L);
-        throw new ClientException();
-//        } catch (Exception e) {
-//            log.info("Catching exception from ClientController");
-//            throw new ClientException();
-//        }
+    @GetMapping
+    public void doSomething(@RequestBody ClientDto clientDto) {
+        producer.send(1L);
     }
 
 }
